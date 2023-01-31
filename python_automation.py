8bbc0d9086d240a69bfc88bc7e1c195ae40832fe
Swarali <kulswaralip@gmail.com>
2023-02-01 00:08:03 +0530

    first commit

---
+++ python_automation.py
@@ -0,0 +1,23 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart,Reference
+
+def process_wb(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    #cell = sheet['a1']
+    #cell = sheet.cell(1,1)
+    #print(cell.value)
+    #print(sheet.max_row)
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        cp = cell.value * 0.9
+        cp_cell = sheet.cell(row, 4)
+        cp_cell.value = cp
+
+    values = Reference(sheet, 
+            min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+    wb.save(filename)
